244e0cb04e3367e26965a5503484bc0acfcc0f04
fs.as.8585@gmail.com <41293675+AndreiyMarkusheuski@users.noreply.github.com>
2023-05-14 17:29:06 +0300

    Module 8: Integration with SQL Database

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Delete, Put, Body, Req, Post, UseGuards, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Delete,
+  Put,
+  Body,
+  Req,
+  Post,
+  HttpStatus,
+} from '@nestjs/common';
 
 // import { BasicAuthGuard, JwtAuthGuard } from '../auth';
 import { OrderService } from '../order';
@@ -11,82 +20,115 @@ import { CartService } from './services';
 export class CartController {
   constructor(
     private cartService: CartService,
-    private orderService: OrderService
-  ) { }
+    private orderService: OrderService,
+  ) {}
 
   // @UseGuards(JwtAuthGuard)
   // @UseGuards(BasicAuthGuard)
   @Get()
-  findUserCart(@Req() req: AppRequest) {
-    const cart = this.cartService.findOrCreateByUserId(getUserIdFromRequest(req));
+  async findUserCart(@Req() req: AppRequest) {
+    try {
+      const cart = await this.cartService.findOrCreateByUserId(
+        getUserIdFromRequest(req),
+      );
 
-    return {
-      statusCode: HttpStatus.OK,
-      message: 'OK',
-      data: { cart, total: calculateCartTotal(cart) },
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'OK',
+        data: { ...cart, total: calculateCartTotal(cart) },
+      };
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: error,
+      };
     }
   }
 
   // @UseGuards(JwtAuthGuard)
   // @UseGuards(BasicAuthGuard)
   @Put()
-  updateUserCart(@Req() req: AppRequest, @Body() body) { // TODO: validate body payload...
-    const cart = this.cartService.updateByUserId(getUserIdFromRequest(req), body)
+  async updateUserCart(@Req() req: AppRequest, @Body() body) {
+    try {
+      const cart = await this.cartService.updateByUserId(
+        getUserIdFromRequest(req),
+        body,
+      );
 
-    return {
-      statusCode: HttpStatus.OK,
-      message: 'OK',
-      data: {
-        cart,
-        total: calculateCartTotal(cart),
-      }
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'OK',
+        data: {
+          cart,
+          total: calculateCartTotal(cart),
+        },
+      };
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: error,
+      };
     }
   }
 
   // @UseGuards(JwtAuthGuard)
   // @UseGuards(BasicAuthGuard)
   @Delete()
-  clearUserCart(@Req() req: AppRequest) {
-    this.cartService.removeByUserId(getUserIdFromRequest(req));
+  async clearUserCart(@Req() req: AppRequest) {
+    try {
+      await this.cartService.removeByUserId(getUserIdFromRequest(req));
 
-    return {
-      statusCode: HttpStatus.OK,
-      message: 'OK',
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'OK',
+      };
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: error,
+      };
     }
   }
 
   // @UseGuards(JwtAuthGuard)
   // @UseGuards(BasicAuthGuard)
   @Post('checkout')
-  checkout(@Req() req: AppRequest, @Body() body) {
-    const userId = getUserIdFromRequest(req);
-    const cart = this.cartService.findByUserId(userId);
+  async checkout(@Req() req: AppRequest, @Body() body) {
+    try {
+      const userId = getUserIdFromRequest(req);
+      const cart = await this.cartService.findByUserId(userId);
 
-    if (!(cart && cart.items.length)) {
-      const statusCode = HttpStatus.BAD_REQUEST;
-      req.statusCode = statusCode
+      if (!(cart && cart.items.length)) {
+        const statusCode = HttpStatus.BAD_REQUEST;
+        req.statusCode = statusCode;
+
+        return {
+          statusCode,
+          message: 'Cart is empty',
+        };
+      }
+
+      const { id: cartId, items } = cart;
+      const total = calculateCartTotal(cart);
+      const order = this.orderService.createByUserId(userId, {
+        ...body,
+        userId,
+        cartId,
+        items,
+        total,
+      });
+      await this.cartService.removeByUserId(userId);
 
       return {
-        statusCode,
-        message: 'Cart is empty',
-      }
-    }
-
-    const { id: cartId, items } = cart;
-    const total = calculateCartTotal(cart);
-    const order = this.orderService.create({
-      ...body, // TODO: validate and pick only necessary data
-      userId,
-      cartId,
-      items,
-      total,
-    });
-    this.cartService.removeByUserId(userId);
-
-    return {
-      statusCode: HttpStatus.OK,
-      message: 'OK',
-      data: { order }
+        statusCode: HttpStatus.OK,
+        message: 'OK',
+        data: { order },
+      };
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: error,
+      };
     }
   }
 }
--- src/cart/services/cart.service.ts
+++ src/cart/services/cart.service.ts
@@ -1,55 +1,115 @@
 import { Injectable } from '@nestjs/common';
-
-import { v4 } from 'uuid';
+import { QueryResult } from 'pg';
+import { client } from '../../db/client';
 
 import { Cart } from '../models';
 
+const DEFAULT_USER_ID = '40e62153-b5c6-4896-987c-f30f3678f608';
+
+export enum CartStatuses {
+  OPEN = 'OPEN',
+  ORDERED = 'ORDERED',
+}
+
 @Injectable()
 export class CartService {
-  private userCarts: Record<string, Cart> = {};
+  async findByUserId(userId: string): Promise<Cart> {
+    const id = userId || DEFAULT_USER_ID;
 
-  findByUserId(userId: string): Cart {
-    return this.userCarts[ userId ];
+    try {
+      const queryCartsText = `select * from carts where user_id = '${id}'`;
+      const result: QueryResult = await client(queryCartsText);
+
+      if (result.rows.length === 0) {
+        return null;
+      }
+
+      const cart = result.rows[0];
+
+      const queryItemsText = `select * from cart_items where cart_id = '${cart.id}'`;
+      const items = (await client(queryItemsText)).rows;
+
+      cart.items = items.map(item => {
+        item.product = {
+          id: item.product_id,
+        };
+        return item;
+      });
+
+      return cart;
+    } catch (err) {
+      console.log('findByUserId: ', err);
+      return err;
+    }
   }
 
-  createByUserId(userId: string) {
-    const id = v4(v4());
-    const userCart = {
-      id,
-      items: [],
-    };
+  async createByUserId(userId: string): Promise<Cart> {
+    const id = userId || DEFAULT_USER_ID;
+    const createdAt = new Date().toJSON();
+    const modifiedAt = new Date().toJSON();
 
-    this.userCarts[ userId ] = userCart;
+    const columns = 'user_id, created_at, updated_at, status';
+    const values = `'${id}', '${createdAt}', '${modifiedAt}', '${CartStatuses.OPEN}'`;
+    const queryText = `INSERT INTO carts(${columns}) VALUES(${values}) RETURNING *`;
 
-    return userCart;
+    try {
+      const result: QueryResult = await client(queryText);
+      if (result?.rows) {
+        return result?.rows[0] as Cart;
+      }
+      return result as Cart;
+    } catch (err) {
+      console.log('createByUserId:', err);
+      return err;
+    }
   }
 
-  findOrCreateByUserId(userId: string): Cart {
-    const userCart = this.findByUserId(userId);
+  async findOrCreateByUserId(userId: string): Promise<Cart> {
+    const userCart = await this.findByUserId(userId || DEFAULT_USER_ID);
 
+    console.log('userCart', userCart);
     if (userCart) {
       return userCart;
     }
-
-    return this.createByUserId(userId);
+    const result: QueryResult = await this.createByUserId(userId);
+    return result;
   }
 
-  updateByUserId(userId: string, { items }: Cart): Cart {
-    const { id, ...rest } = this.findOrCreateByUserId(userId);
+  async updateByUserId(userId: string, req): Promise<Cart> {
+    const { items } = JSON.parse(req.toString());
+
+    const { id, ...rest } = await this.findOrCreateByUserId(userId);
 
     const updatedCart = {
       id,
       ...rest,
-      items: [ ...items ],
+      items: [...items, ...rest.items],
+    };
+
+    try {
+      await Promise.all(
+        items.map(item => {
+          const values = `'${id}', '${item.count}', '${item.product.id}'`;
+          const queryText = `insert into cart_items (cart_id, count, product_id) values(${values})`;
+          return client(queryText);
+        }),
+      );
+
+      return { ...updatedCart };
+    } catch (err) {
+      console.log('updateByUserId:', err);
+      return err;
     }
-
-    this.userCarts[ userId ] = { ...updatedCart };
-
-    return { ...updatedCart };
   }
 
-  removeByUserId(userId): void {
-    this.userCarts[ userId ] = null;
+  async removeByUserId(userId): Promise<void> {
+    try {
+      const { id } = await this.findOrCreateByUserId(userId);
+      const queryText = `DELETE FROM cart_items WHERE cart_id = '${id}'`;
+      await client(queryText);
+    } catch (err) {
+      console.log('removeByUserId:', err);
+      return err;
+    }
   }
-
 }
